f94190ac13f2bca8a8f6ecbbb48aa45b0d1ed247
yasindurgun <yasin.durgun52@gmail.com>
2021-12-22 20:20:41 +0300

    Post Repository created

---
+++ BlogApp/Repositories/PostRepository.cs
@@ -0,0 +1,21 @@
+﻿using BlogApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.Repositories
+{
+    public class PostRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public PostRepository()
+        {
+            _db = new ApplicationDbContext();
+        }
+         public List<Post> List()
+        {
+            return _db.Posts.ToList();
+        }
+    }
+}
